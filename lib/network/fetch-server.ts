de77673a87159885dfe615950511f83d74bf16f6
Nicolas Oleinizak <nicolas.oleinizak@gmail.com>
2024-04-27 08:56:58 -0300

    feat: use API authentication header

--- lib/network/fetch-server.ts
+++ lib/network/fetch-server.ts
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export const fetchServer = axios.create({
     baseURL: process.env.NEXT_API_BASE_URL,
+    headers: {
+        'X-API-KEY': process.env.NEXT_API_KEY,
+    }
 });
